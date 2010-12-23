0b8f8137fb823abc53befa651f06da28aba41ab4
Sorin Ionescu <sorin.ionescu@gmail.com>
2010-12-23 12:22:20 -0500

    Update Homebrew formula

--- extras/attach.rb
+++ extras/attach.rb
@@ -1,13 +1,15 @@
 require 'formula'
 
-class Attach <Formula
-  url 'git://github.com/SpookyET/attach.git' :tag => '1.0.0'
-  version 1.0.0
+class Attach < Formula
+  url 'git://github.com/SpookyET/attach.git', :tag => '1.0.0'
+  head 'git://github.com/SpookyET/attach.git'
+  homepage 'http://github.com/SpookyET/attach'
+  version '1.0.0'
   depends_on 'dtach'
 
   def install
-    bin.install "attach"
-    man.install "attach.1"
+    bin.install 'attach'
+    man1.install 'attach.1'
   end
 end
 
