142786002fbcf79869f1e83290e68d57e0c826d6
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-09-29 20:21:30 -0400

    Set the shell title with the session name

--- extras/attach.rb
+++ extras/attach.rb
@@ -1,10 +1,10 @@
 require 'formula'
 
 class Attach < Formula
-  url 'https://github.com/sorin-ionescu/attach.git', :tag => '1.0.4'
+  url 'https://github.com/sorin-ionescu/attach.git', :tag => '1.0.5'
   head 'https://github.com/sorin-ionescu/attach.git'
   homepage 'http://github.com/sorin-ionescu/attach'
-  version '1.0.4'
+  version '1.0.5'
   depends_on 'dtach'
 
   def install
