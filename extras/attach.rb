a3b7408d4b66fffc19c8f951acbf7363518ac680
Sorin Ionescu <sorin.ionescu@gmail.com>
2011-12-30 15:49:50 -0500

    Update the Homebrew formula to the standard

--- extras/attach.rb
+++ extras/attach.rb
@@ -1,10 +1,11 @@
 require 'formula'
 
 class Attach < Formula
-  url 'https://github.com/sorin-ionescu/attach.git', :tag => '1.0.5'
+  url 'https://github.com/sorin-ionescu/attach/tarball/1.0.5'
   head 'https://github.com/sorin-ionescu/attach.git'
   homepage 'http://github.com/sorin-ionescu/attach'
-  version '1.0.5'
+  md5 '351d3ad4356513fe8a6bed05696726bb'
+
   depends_on 'dtach'
 
   def install
